6df02c9fa769eb9884ed7f919eda05bb93cb35e0
Ben Powley <ben.powley97@gmail.com>
2018-07-30 21:53:59 +0100

    Add toggle todo action to reducer. Update add todo action to include id

--- src/store/reducers/todoReducer.js
+++ src/store/reducers/todoReducer.js
@@ -1,21 +1,31 @@
-import { AddTodo } from "../actions";
-
 const initialState = {
     todos: []
 };
 
 const todoReducer = (state = initialState, action) => {
+    console.log(action);
     switch (action.type) {
-        case AddTodo:
-            return Object.assign({}, state, {
+        case "ADD_TODO":
+            return {
                 todos: [
                     ...state.todos,
                     {
+                        id: action.id,
                         text: action.text,
                         completed: false
                     }
                 ]
-            });
+            };
+
+        case "TOGGLE_TODO":
+            const todos = state.todos.map(todo => ({
+                ...todo,
+                completed: todo.id === action.id ? !todo.completed : todo.completed
+            }));
+
+            return {
+                todos: [...todos]
+            };
 
         default:
             return state;
